0f2e0d90f88a960ab13353688fe71e47c4c4a7ff
Andrew <aldortheold@gmail.com>
2024-04-23 21:55:14 +1000

    fixed some bugs; added dark mode

---
+++ theme.js
@@ -0,0 +1,30 @@
+const modeToggle = document.getElementById("modeToggle");
+const modeImg = document.getElementById("modeImg");
+let darkMode = localStorage.getItem("darkMode");
+
+function enableDarkMode() {
+    document.body.classList.add("darkMode");
+    localStorage.setItem("darkMode", "enabled");
+    modeImg.src = "img/light_mode.png";
+}
+
+function disableDarkMode() {
+    document.body.classList.remove("darkMode");
+    localStorage.setItem("darkMode", null);
+    modeImg.src = "img/dark_mode.png";
+}
+
+if (darkMode == "enabled") {
+    enableDarkMode();
+    modeImg.src = "img/light_mode.png";
+}
+
+modeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode != "enabled") {
+        enableDarkMode();
+    }
+    else {
+        disableDarkMode();
+    }
+})
