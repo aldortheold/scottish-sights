246adaef765395cdde846ed17a0286ea3b499a71
Andrew <aldortheold@gmail.com>
2024-04-26 11:08:04 +1000

    improvement #2

--- theme.js
+++ theme.js
@@ -6,17 +6,20 @@ function enableDarkMode() {
     document.body.classList.add("darkMode");
     localStorage.setItem("darkMode", "enabled");
     modeImg.src = "img/dark_mode.png";
+    modeImg.alt = "🌚"
 }
 
 function disableDarkMode() {
     document.body.classList.remove("darkMode");
     localStorage.setItem("darkMode", null);
     modeImg.src = "img/light_mode.png";
+    modeImg.alt = "🌞"
 }
 
 if (darkMode == "enabled") {
     enableDarkMode();
     modeImg.src = "img/dark_mode.png";
+    modeImg.alt = "🌚"
 }
 
 modeToggle.addEventListener("click", () => {
